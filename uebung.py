448189cd3b29e915c196077054b253f19299c2b0
Lambi11 <joshualambert2202@gmail.com>
2022-06-09 11:05:44 +0200

    Neues Beispiel hinzugefügt

--- uebung.py
+++ uebung.py
@@ -1,9 +1,15 @@
 import re
 
-txt = 'The rain in Spain'
-x = re.search('ai', txt)
-print(x)
+#txt = 'The rain in Spain'
+#x = re.search('ai', txt)
+#print(x)
 
-txt = 'The rain in Spain'
-x = re.findall('ai', txt)
-print(x)
+#txt = 'The rain in Spain'
+#x = re.findall('ai', txt)
+#print(x)
+
+txt = "Ich habe 1 Präsentation zum Thema REGEX erstellt"
+result = re.search(r"(\b\d+).+(\b[A-Z]+\b)", txt)
+print(result.groups()) 
+print(result.group(1))
+print(result.group(2))

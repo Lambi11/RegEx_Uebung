fc06c15ee23e4e10f766c7e5eb92401d4ad8489a
Lambi11 <joshualambert2202@gmail.com>
2022-06-09 14:32:26 +0200

    Beispiel Email hinzugefügt

--- uebung.py
+++ uebung.py
@@ -13,3 +13,12 @@ result = re.search(r"(\b\d+).+(\b[A-Z]+\b)", txt)
 print(result.groups()) 
 print(result.group(1))
 print(result.group(2))
+
+
+#Benutzer E-Mail
+pattern = "[(a-z)(A-Z)(0-9)]+@[(a-z)(A-Z)]+\.(com|de)"
+user_input = input('Enter your email: ')
+if(re.search(pattern, user_input)):
+    print('valid email')
+else:
+    print('invalid email')    

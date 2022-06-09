b87dc96d1c9c7b822625e1265640ad985e0d73d8
Lambi11 <joshualambert2202@gmail.com>
2022-06-09 14:33:59 +0200

    vorheriges Beispiel gelöscht

--- uebung.py
+++ uebung.py
@@ -1,13 +1,5 @@
 import re
 
-#txt = 'The rain in Spain'
-#x = re.search('ai', txt)
-#print(x)
-
-#txt = 'The rain in Spain'
-#x = re.findall('ai', txt)
-#print(x)
-
 txt = "Ich habe 1 Präsentation zum Thema REGEX erstellt"
 result = re.search(r"(\b\d+).+(\b[A-Z]+\b)", txt)
 print(result.groups()) 
